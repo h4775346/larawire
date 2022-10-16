fc6b05318664c2c330263c572c8db853efb26429
h4775346 <englishh7366@gmail.com>
2022-10-16 17:14:56 +0200

    Dark Mode For Authentication Pages Added 🔥🔥

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-800">
+<div class="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-800">
 
     <div>
         {{ $logo }}
