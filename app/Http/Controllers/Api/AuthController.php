26664303674fee65ef88ef0b8470621f5427e38d
h4775346 <englishh7366@gmail.com>
2022-10-29 22:59:48 +0200

    Api Auth With Permissions Added 🔥🔥🔥

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Spatie\Permission\Models\Permission;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->fail('Invalid Email Or Password', 400);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->success_data(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->success('Successfully logged out');
+    }
+
+
+    /**
+     * Get the token array structure.
+     *
+     * @param string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken(string $token)
+    {
+        $user = Auth::user();
+
+        return response()->success_object(['token' => $token, 'permissions'=>$user->permissions_name()]);
+    }
+}
